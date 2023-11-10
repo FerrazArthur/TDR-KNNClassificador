d5f3275873625a06899aacf99c4f7b70cafb8630
Arthur Ferraz <arthursnc@gmail.com>
2023-11-10 13:08:00 -0300

    [feat] added function to print confusion matrix

--- models/predict.py
+++ models/predict.py
@@ -4,13 +4,44 @@ import pandas as pd
 import seaborn as sns
 from data.retrieve import explore_csv_dataframe
 from models.split_train_test import treino_media, treino_regular
-from visualization.visualization import imprimeTestesMultiplos, imprimeTestesMultiplos_train_explicit
+from visualization.visualization import imprime_conf_mat_e_class_rep, imprime_testes_multiplos, imprime_testes_multiplos_train_explicit
+
+def get_confusion(data_dict:dict, treino, test_size:float=0.994, k:int=1, imprime_legenda:bool=True, imprime_accuracy:bool=True):
+    """
+        Realiza o treino do classificador para uma distribuição e para um valor de k
+        e imprime a matriz de confusão e o relatório de classificação.
+    """
+    legenda=[]
+
+    X_train, X_test, y_train, y_test = treino(data_dict, test_size=test_size)
+    knn_class = KNeighborsClassifier(n_neighbors=k, weights='distance')
+    print(f"X_train: {len(X_train)}")
+    print(f"X_test: {len(X_test)}")
+
+    # Treinando o classificador
+    knn_class.fit(X_train, y_train)
+
+    # Realizando o teste
+    ypred=knn_class.predict(X_test)
+    # if imprime_legenda == True:
+    #     # Definindo uma legenda
+    #     legenda = [key for key in data_dict.keys()]
+
+    conf_mat = confusion_matrix(y_test, ypred, labels=legenda)
+    classification_rep = classification_report(y_test, ypred, output_dict=True)
+    
+    acuracia = accuracy_score(y_test,ypred)
+    if imprime_accuracy == True:
+        imprime_conf_mat_e_class_rep(conf_mat, classification_rep, acuracia=acuracia, legenda=legenda)
+    else:
+        imprime_conf_mat_e_class_rep(conf_mat, classification_rep, legenda=legenda)
+    return acuracia
 
 def run_basic(X_train, X_test, y_train, y_test, k=1):
     """
         Run a basic KNN classifier with the given data.
     """
-    knn_class = KNeighborsClassifier(n_neighbors=k)
+    knn_class = KNeighborsClassifier(n_neighbors=k, weights='distance')
 
     #treinando o classificador
     knn_class.fit(X_train, y_train)
@@ -21,26 +52,26 @@ def run_basic(X_train, X_test, y_train, y_test, k=1):
     return accuracy_score(y_test,ypred)
 
 def run_multiple_tests(rangek=[5, 4, 3, 2, 1], distribrange=[0.87, 0.95, 0.99, 0.995],
-                        treinos=[treino_regular, treino_media], path='dados_1_carga'):
+                        treinos=[treino_regular, treino_media], path='dados_1_carga', result_method=get_confusion):
     """
         Run multiple tests with different values of k and different distributions of train and test data.
     """
     # Realiza o treino do classificador para cada distribuição e para cada valor de k
     for treino in treinos:
-        dados = getdata(path, treino, distribrange, rangek)
+        dados = get_data(path, treino, distribrange, rangek, result_method=result_method)
         # Exibe os resultados em uma matriz
-        imprimeTestesMultiplos(dados, distribrange, rangek)
+        imprime_testes_multiplos(dados, distribrange, rangek)
 
-def run_multiple_tests__train_explicit(rangek=[5, 4, 3, 2, 1], trainrange=[10, 15, 30, 60],
+def run_multiple_tests_train_explicit(rangek=[5, 4, 3, 2, 1], trainrange=[10, 15, 30, 60],
                         treinos=[treino_regular, treino_media], path='dados_1_carga'):
     """
         Run multiple tests with different values of k and different distributions of train and test data.
     """
     # Realiza o treino do classificador para cada distribuição e para cada valor de k
     for treino in treinos:
-        dados = getdata_train_explicit(path, treino, trainrange, rangek)
+        dados = get_data_train_explicit(path, treino, trainrange, rangek)
         # Exibe os resultados em uma matriz
-        imprimeTestesMultiplos_train_explicit(dados, trainrange, rangek)
+        imprime_testes_multiplos_train_explicit(dados, trainrange, rangek)
 
 def get_accuracy(data_dict:dict, treino, test_size:float=0.994, k:int=1):
     """
@@ -60,7 +91,7 @@ def get_accuracy_train_explicit(data_dict:dict, treino, train_size:int=30, k:int
     print(f"X_test: {len(X_test)}")
     return run_basic(X_train, X_test, y_train, y_test, k=k)
 
-def getdata(path, treino, distribrange, rangek):
+def get_data(path, treino, distribrange, rangek, result_method=get_accuracy):
     """
         Get the data from the given path and run the tests.
     """
@@ -68,9 +99,9 @@ def getdata(path, treino, distribrange, rangek):
     data_dict = explore_csv_dataframe(path)
     
     # Rodando os testes
-    return [[get_accuracy(data_dict, treino, test_size, k) for test_size in distribrange] for k in rangek]
+    return [[result_method(data_dict, treino, test_size, k) for test_size in distribrange] for k in rangek]
 
-def getdata_train_explicit_old(path, treino, trainrange, rangek):
+def get_data_train_explicit_old(path, treino, trainrange, rangek):
     """
         Get the data from the given path and run the tests.
     """
@@ -80,7 +111,7 @@ def getdata_train_explicit_old(path, treino, trainrange, rangek):
     # Rodando os testes
     return [[get_accuracy_train_explicit(data_dict, treino, train_size, k) for train_size in trainrange] for k in rangek]
 
-def getdata_train_explicit(path, treino, trainrange, rangek, num_tests=10):
+def get_data_train_explicit(path, treino, trainrange, rangek, num_tests=10):
     """
     Get the data from the given path and run tests, returning the average of X tests with the same configuration.
     """
--- visualization/visualization.py
+++ visualization/visualization.py
@@ -1,5 +1,33 @@
 from math import floor
 import tkinter as tk
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report, accuracy_score
+import pandas as pd
+import seaborn as sns
+
+def imprime_conf_mat_e_class_rep(conf_mat, classification_rep, legenda=[], acuracia=None):
+    """
+    Imprime a matriz de confusão e o relatório de classificação em um 
+    plot único
+    """
+
+    # if len(legenda) > 0:
+    #     print("legenda: ")
+    #     for i in range(len(legenda)):
+    #         print(i+1, ": ",legenda[i])
+    if acuracia != None:
+        print("Accuracy:",acuracia)
+        
+    cmd = ConfusionMatrixDisplay(confusion_matrix=conf_mat, display_labels=legenda)
+    cmd.plot(values_format="d")
+
+    plt.show()
+    # fig, ax = plt.subplots(1, 1)
+    # cmd = ConfusionMatrixDisplay(confusion_matrix=conf_mat, display_labels=legenda)
+    # cmd.plot(values_format="d", ax=ax[0])
+    sns.heatmap(pd.DataFrame(classification_rep).iloc[:-1, :].T, annot=True)
+    plt.show()
+
 
 def showMat(matriz, labelx=[], labely=[]):
     """
@@ -25,7 +53,7 @@ def showMat(matriz, labelx=[], labely=[]):
             label.grid(row=i+1, column=j+1, padx=5, pady=5)
     janela.mainloop()
 
-def imprimeTestesMultiplos(matriz, distribrange, rangek):
+def imprime_testes_multiplos(matriz, distribrange, rangek):
     """
     Essa função cria nomes para rotular as linhas e colunas da matriz que será exibida.
     """
@@ -42,7 +70,7 @@ def imprimeTestesMultiplos(matriz, distribrange, rangek):
     # Passa a matriz, os rótulos dos eixos x e y para a função que desenha a matriz numa janela
     showMat(matriz, labelx, labely)
 
-def imprimeTestesMultiplos_train_explicit(matriz, trainrange, rangek):
+def imprime_testes_multiplos_train_explicit(matriz, trainrange, rangek):
     """
     Essa função cria nomes para rotular as linhas e colunas da matriz que será exibida.
     """

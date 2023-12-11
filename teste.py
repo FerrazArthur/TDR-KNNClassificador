1e57e1314872d09208416dc261ef668d81e0acde
Arthur Ferraz <arthursnc@gmail.com>
2023-12-11 06:39:23 -0300

    [env] Remove old files

--- teste.py
+++ teste.py
@@ -15,92 +15,31 @@ corre_cruzada_pasta = "correlacao_cruzada"
 corre_simples_pasta = "correlacao_simples"
 knn_pasta= "knn"
 
-# conjunto_dados = Dados(conjunto1_nome)
-# conjunto_dados = Dados('2_cargas_3_dp')
-# pior_classe, _, _ = obter_linha_maior_distancia_minkowski_entre_dataframes(conjunto_dados.dicionario_dados, p=2)
-# imprime_distribuicao_padronizada_distancias(obter_vetor_distancias_a_media_dataframe(conjunto_dados.dicionario_dados[pior_classe], p=2))
-# imprime_distancias(obter_vetor_distancias_a_media_dataframe(conjunto_dados.dicionario_dados[pior_classe], p=2))
-# print(conjunto_dados.dicionario_dados[pior_classe].shape)
+resultdict_1={}
+conjunto_dados = Dados(conjunto1_nome)
+conjunto_dados.reduzir_dimensionalidade(extended_slices=2)
+resultdict_1["CS"] = executar_multiplas_previsoes_correlacao_matriz_confusao(conjunto_dados, classificador=ClassificadorCorrelacao, tamanho_treino_lista=[32, 64, 120, 240, 480], repeticoes=20, save_fig=True, fig_folder=str(conjunto1_nome / corre_simples_pasta))
+resultdict_1["CA"] = executar_multiplas_previsoes_correlacao_matriz_confusao(conjunto_dados, classificador=ClassificadorCorrelacaoCruzada, tamanho_treino_lista=[32, 64, 120, 240, 480], repeticoes=20, save_fig=True, fig_folder=str(conjunto1_nome / corre_cruzada_pasta))
+resultdict_1["kNN"] = executar_multiplas_previsoes_KNN_matriz_confusao(conjunto_dados, [1, 3, 5], [32, 64, 120, 240, 480], treinos_lista=[treino_regular, treino_media], repeticoes=20, save_fig=True, fig_folder=str(conjunto1_nome / knn_pasta))
+
+print(resultdict_1)
+# Salvar o dicionário em um arquivo
+with open('dict_1_500.pkl', 'wb') as f:
+    pickle.dump(resultdict_1, f)
+
+resultdict_2={}
+conjunto_dados = Dados(conjunto2_nome, base_codigo=8)
+conjunto_dados.reduzir_dimensionalidade(extended_slices=2)
+resultdict_2["CS"] = executar_multiplas_previsoes_correlacao_matriz_confusao(conjunto_dados, classificador=ClassificadorCorrelacao, tamanho_treino_lista=[630, 1050, 1260, 1470, 1932], repeticoes=20, save_fig=True, fig_folder=str(conjunto2_nome / corre_simples_pasta))
+resultdict_2["CA"] = executar_multiplas_previsoes_correlacao_matriz_confusao(conjunto_dados, classificador=ClassificadorCorrelacaoCruzada, tamanho_treino_lista=[630, 1050, 1260, 1470, 1932], repeticoes=20, save_fig=True, fig_folder=str(conjunto2_nome / corre_cruzada_pasta))
+resultdict_2["kNN"] = executar_multiplas_previsoes_KNN_matriz_confusao(conjunto_dados, [1, 3, 5], [630, 1050, 1260, 1470, 1932], treinos_lista=[treino_regular, treino_media], repeticoes=20, save_fig=True, fig_folder=str(conjunto2_nome / knn_pasta))
 
 
-# tamanho_treino_lista= [32]
-# conjunto_dados = Dados(conjunto1_nome)
-# resultdict = {}
-# resultdict["KNN"] = executar_multiplas_previsoes_KNN_matriz_confusao(conjunto_dados, [1, 3], [32, 64], treinos_lista=[treino_regular, treino_media], repeticoes=1, save_fig=True, fig_folder=str(conjunto1_nome / knn_pasta))
+print(resultdict_2)
+# Salvar o dicionário em um arquivo
+with open('dict_2_500.pkl', 'wb') as f:
+    pickle.dump(resultdict_2, f)
 
-# resultdict["CC"] = executar_multiplas_previsoes_correlacao_matriz_confusao(conjunto_dados, classificador=ClassificadorCorrelacao, tamanho_treino_lista=tamanho_treino_lista, repeticoes=1, save_fig=True, fig_folder=str(conjunto1_nome / corre_simples_pasta))
-# print(resultdict)
-# # resultdict["CA"] = executar_multiplas_previsoes_correlacao_matriz_confusao(conjunto_dados, classificador=ClassificadorCorrelacaoCruzada, tamanho_treino_lista=[32], repeticoes=1, save_fig=True, fig_folder=str(conjunto1_nome / corre_cruzada_pasta))
-
-# resultdict["KNN"] = executar_multiplas_previsoes_KNN_matriz_confusao(conjunto_dados, [1, 3], [32, 64], treinos_lista=[treino_regular, treino_media], repeticoes=1, save_fig=True, fig_folder=str(conjunto1_nome / knn_pasta))
-
-# conjunto_dados = Dados(conjunto2_nome)
-# executar_multiplas_previsoes_correlacao_matriz_confusao(conjunto_dados, classificador=ClassificadorCorrelacao, tamanho_treino_lista=[630], repeticoes=1, save_fig=True, fig_folder=str(conjunto2_nome / corre_simples_pasta))
-# executar_multiplas_previsoes_correlacao_matriz_confusao(conjunto_dados, classificador=ClassificadorCorrelacaoCruzada, tamanho_treino_lista=[630], repeticoes=1, save_fig=True, fig_folder=str(conjunto2_nome / corre_cruzada_pasta))
-# executar_multiplas_previsoes_KNN_matriz_confusao(conjunto_dados, [1], [630], treinos_lista=[treino_media], repeticoes=1, save_fig=True, fig_folder=str(conjunto2_nome / knn_pasta))
-
-
-
-# resultdict_1={}
-# conjunto_dados = Dados(conjunto1_nome)
-# conjunto_dados.reduzir_dimensionalidade(extended_slices=2)
-# resultdict_1["CS"] = executar_multiplas_previsoes_correlacao_matriz_confusao(conjunto_dados, classificador=ClassificadorCorrelacao, tamanho_treino_lista=[32, 64, 120, 240, 480], repeticoes=20, save_fig=True, fig_folder=str(conjunto1_nome / corre_simples_pasta))
-# resultdict_1["CA"] = executar_multiplas_previsoes_correlacao_matriz_confusao(conjunto_dados, classificador=ClassificadorCorrelacaoCruzada, tamanho_treino_lista=[32, 64, 120, 240, 480], repeticoes=20, save_fig=True, fig_folder=str(conjunto1_nome / corre_cruzada_pasta))
-# resultdict_1["kNN"] = executar_multiplas_previsoes_KNN_matriz_confusao(conjunto_dados, [1, 3, 5], [32, 64, 120, 240, 480], treinos_lista=[treino_regular, treino_media], repeticoes=20, save_fig=True, fig_folder=str(conjunto1_nome / knn_pasta))
-
-# print(resultdict_1)
-# # Salvar o dicionário em um arquivo
-# with open('dict_1_500.pkl', 'wb') as f:
-#     pickle.dump(resultdict_1, f)
-
-# resultdict_2={}
-# conjunto_dados = Dados(conjunto2_nome, base_codigo=8)
-# conjunto_dados.reduzir_dimensionalidade(extended_slices=2)
-# resultdict_2["CS"] = executar_multiplas_previsoes_correlacao_matriz_confusao(conjunto_dados, classificador=ClassificadorCorrelacao, tamanho_treino_lista=[630, 1050, 1260, 1470, 1932], repeticoes=20, save_fig=True, fig_folder=str(conjunto2_nome / corre_simples_pasta))
-# resultdict_2["CA"] = executar_multiplas_previsoes_correlacao_matriz_confusao(conjunto_dados, classificador=ClassificadorCorrelacaoCruzada, tamanho_treino_lista=[630, 1050, 1260, 1470, 1932], repeticoes=20, save_fig=True, fig_folder=str(conjunto2_nome / corre_cruzada_pasta))
-# resultdict_2["kNN"] = executar_multiplas_previsoes_KNN_matriz_confusao(conjunto_dados, [1, 3, 5], [630, 1050, 1260, 1470, 1932], treinos_lista=[treino_regular, treino_media], repeticoes=20, save_fig=True, fig_folder=str(conjunto2_nome / knn_pasta))
-
-
-# print(resultdict_2)
-# # Salvar o dicionário em um arquivo
-# with open('dict_2_500.pkl', 'wb') as f:
-#     pickle.dump(resultdict_2, f)
-
-# dict1="dict_1_500.pkl"
-# dict2="dict_2_500.pkl"
-# imprime_tabela_latex_precisao(dict1_caminho=dict1, dict2_caminho=dict2)
-
-
-
-# conjunto_dados.reduzir_dimensionalidade(extended_slices=2)
-
-# imprima a quantidade de colunas no novo conjunto de dados
-# conjunto_dados.imprime_classes(caminho=conjunto1_nome, save_fig=True, nome_arquivo="EXP_1_RESULT.pdf")
-# conjunto_dados.imprime_classes(caminho=conjunto1_nome, save_fig=True, nome_arquivo="EXP_1_MEANS.pdf", imprimir_medias=True)
-
-# imprime_matriz_distancias_classes(obter_distancia_minkowski_entre_classes(conjunto_dados.dicionario_dados, p=2), save_fig=True, caminho=conjunto1_nome, legendas=conjunto_dados.legenda)
-# imprime_distribuicao_distancias(obter_distancia_minkowski_min_mean_max_em_classes(conjunto_dados.dicionario_dados, p=2), save_fig=True, caminho=conjunto1_nome)
-
-# executar_multiplas_previsoes_KNN_matriz_confusao(conjunto_dados, [1], [40], treinos_lista=[ treino_media], repeticoes=1, save_fig=True, fig_folder=str(conjunto1_nome / knn_pasta))
-
-# conjunto_dados = Dados("2_cargas_raw", base_codigo=8)
-# for nome, df in conjunto_dados.dicionario_dados.items():
-#     print(f"{conjunto_dados.legenda[nome]} & {nome} & & & {df.shape[0]}\\\\")
-
-# # conjunto_dados.reduzir_dimensionalidade(extended_slices=2)
-# conjunto_dados.imprime_classes(caminho=conjunto2_nome, save_fig=True, nome_arquivo="EXP_2_RESULT.pdf", frequencia=2)
-# conjunto_dados.imprime_classes(caminho=conjunto2_nome, save_fig=True, nome_arquivo="EXP_2_MEANS.pdf", imprimir_medias=True, frequencia=2)
-
-# imprime_matriz_distancias_classes(obter_distancia_minkowski_entre_classes(conjunto_dados.dicionario_dados, p=2), save_fig=True, caminho=conjunto2_nome, legendas=conjunto_dados.legenda)
-# imprime_distribuicao_distancias(obter_distancia_minkowski_min_mean_max_em_classes(conjunto_dados.dicionario_dados, p=2), save_fig=True, caminho=conjunto2_nome)
-
-# executar_multiplas_previsoes_KNN_matriz_confusao(conjunto_dados, [1], [651], treinos_lista=[treino_media], repeticoes=1, save_fig=True, fig_folder=str(conjunto2_nome / knn_pasta))
-
-# Cargas_1_raw = Dados("1_carga_raw")
-# Cargas_1_raw.imprime_classes(caminho="1_carga_raw", save_fig=False, nome_arquivo="EXP_1_RESULT.pdf")
-# Cargas_1_raw.imprime_classes(caminho="1_carga_raw", lista_classes=["Circuito aberto", "Curto Circuito"], save_fig=True, nome_arquivo="EXP1_abertoFechado.pdf")
-# Cargas_1_raw.imprime_classes(caminho="1_carga_raw", save_fig=True, nome_arquivo="EXP_1_CUT.pdf", inicio_plot=0.9*(10**-7))
-
-# Cargas_2_raw = Dados("2_cargas_raw", base_codigo=8)
-# Cargas_2_raw.imprime_classes(caminho="2_cargas_raw", save_fig=True, nome_arquivo="EXP_2_RESULT.pdf", frequencia=2)
-# Cargas_2_raw.imprime_classes(caminho="2_cargas_raw", lista_classes=["CA CA", "CA CC", "CC CA"], save_fig=True, nome_arquivo="EXP_2_2_2.pdf", frequencia=2)
+dict1="dict_1_500.pkl"
+dict2="dict_2_500.pkl"
+imprime_tabela_latex_precisao(dict1_caminho=dict1, dict2_caminho=dict2)
